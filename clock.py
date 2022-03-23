214d467848aa465da16fbf23a652ea805d67bb3a
Atom <chanonatom@gmail.com>
2022-03-24 04:51:34 +0700

    test time

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ from app import dailyFunc
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 scheduler = BlockingScheduler()
-@scheduler.scheduled_job('cron', id='fetch_daily', day='*',hour='9', minute='0')
+@scheduler.scheduled_job('cron', id='fetch_daily', day='*',hour='4', minute='55')
 def update_func():
     print("start update")
     dailyFunc()
