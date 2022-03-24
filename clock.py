57c642e5e0c5efc68a00b15f6e73a70e4ebfa659
Atom <chanonatom@gmail.com>
2022-03-24 19:31:41 +0700

    change clock

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from app import dailyFunc
 
 # scheduler = BlockingScheduler()
 # @scheduler.scheduled_job('cron', id='fetch_daily', day='*',hour='5', minute='0')
-def update_func():
-    print("start update")
-    dailyFunc()
-    print("finish update")
+
+print("start update")
+dailyFunc()
+print("finish update")
