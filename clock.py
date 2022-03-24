c5e226f2efe08ec9c293366ced9f4b119ed63de9
Atom <chanonatom@gmail.com>
2022-03-24 18:48:43 +0700

    add update fuct

--- clock.py
+++ clock.py
@@ -1,9 +1,9 @@
-# from app import dailyFunc
+from app import dailyFunc
 
 
 # scheduler = BlockingScheduler()
 # @scheduler.scheduled_job('cron', id='fetch_daily', day='*',hour='5', minute='0')
-# def update_func():
-#     print("start update")
-#     # dailyFunc()
-#     print("finish update")
+def update_func():
+    print("start update")
+    dailyFunc()
+    print("finish update")
